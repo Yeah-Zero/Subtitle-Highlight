b56fd0ad0e1e96c183ef4c35762f57935e37de39
烨凌 <92421089+Iekrin@users.noreply.github.com>
2022-10-10 15:42:37 +0800

    提交

--- src/main/java/Iekrin/ExampleMod/Configure/Configuration.java
+++ src/main/java/Iekrin/ExampleMod/Configure/Configuration.java
@@ -1,4 +1,18 @@
 package Iekrin.ExampleMod.Configure;
 
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+
 public class Configuration {
+    private static final File 文件 = new File(FabricLoader.getInstance().getConfigDir().toFile().getPath() + "\\配置文件.json");
+    public static String 字符串 = "初始值";
+
+    public static void 保存() {
+
+    }
+
+    public static void 读取() {
+
+    }
 }
--- src/main/java/Iekrin/ExampleMod/Configure/Screen.java
+++ src/main/java/Iekrin/ExampleMod/Configure/Screen.java
@@ -1,4 +1,19 @@
 package Iekrin.ExampleMod.Configure;
 
-public class Screen {
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import net.minecraft.text.Text;
+
+public class Screen implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return (上级界面) -> {
+            ConfigBuilder 构建器 = ConfigBuilder.create().setParentScreen(上级界面).setTitle(Text.literal("配置屏幕")).setDoesConfirmSave(false);
+            构建器.getOrCreateCategory(Text.literal("分类名称")).addEntry(构建器.entryBuilder().startStrField(Text.literal("选项"), Configuration.字符串).setDefaultValue("默认值").setTooltip(Text.literal("提示")).setSaveConsumer((新值) -> {
+                Configuration.字符串 = 新值;
+            }).build());
+            return 构建器.build();
+        };
+    }
 }
