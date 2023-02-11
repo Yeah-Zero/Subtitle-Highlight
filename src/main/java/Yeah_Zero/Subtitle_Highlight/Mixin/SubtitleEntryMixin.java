76375b5a44d2045d4cfd28de3bf2a67b0b273ad7
烨凌 <92421089+Yeah-Zero@users.noreply.github.com>
2023-02-11 12:00:22 +0800

    提交

--- src/main/java/Yeah_Zero/Subtitle_Highlight/Mixin/SubtitleEntryMixin.java
+++ src/main/java/Yeah_Zero/Subtitle_Highlight/Mixin/SubtitleEntryMixin.java
@@ -42,167 +42,168 @@ public class SubtitleEntryMixin {
 
     @Inject(at = @At("RETURN"), method = "getText()Lnet/minecraft/text/Text;", cancellable = true)
     private void 字幕着色(CallbackInfoReturnable<Text> 可返回回调信息) {
-        if (this.text.getContent() instanceof TranslatableTextContent) {
+        MutableText 字幕文本 = ((MutableText) this.text).formatted(Formatting.RESET);
+        if (字幕文本.getContent() instanceof TranslatableTextContent) {
             for (Settings.Custom 元素 : Configuration.配置项.自定义列表) {
-                if (((TranslatableTextContent) this.text.getContent()).getKey().equals(元素.本地化键名)) {
-                    可返回回调信息.setReturnValue(((MutableText) this.text).setStyle(this.text.getStyle().withColor(元素.颜色).withObfuscated(元素.随机).withBold(元素.粗体).withStrikethrough(元素.删除线).withUnderline(元素.下划线).withItalic(元素.斜体)));
+                if (((TranslatableTextContent) 字幕文本.getContent()).getKey().equals(元素.本地化键名)) {
+                    可返回回调信息.setReturnValue(字幕文本.setStyle(字幕文本.getStyle().withColor(元素.颜色).withObfuscated(元素.随机).withBold(元素.粗体).withStrikethrough(元素.删除线).withUnderline(元素.下划线).withItalic(元素.斜体)));
                     return;
                 }
             }
-            String[] 键名分割 = ((TranslatableTextContent) this.text.getContent()).getKey().split("\\.");
+            String[] 键名分割 = ((TranslatableTextContent) 字幕文本.getContent()).getKey().split("\\.");
             if (键名分割[0].equals("subtitles")) {
                 switch (键名分割[1]) {
                     case "ambient", "weather" -> {
-                        可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.环境.获取格式代码()));
+                        可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.环境.获取格式代码()));
                         return;
                     }
                     case "block" -> {
                         if (键名分割[2].equals("generic")) {
-                            可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.通用.获取格式代码()));
+                            可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.通用.获取格式代码()));
                             return;
                         }
                         for (String 元素 : 互动) {
                             if (键名分割[2].equals(元素)) {
                                 if (键名分割[2].equals("anvil") && 键名分割[3].equals("land")) {
-                                    可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.危险方块.获取格式代码()));
+                                    可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.危险方块.获取格式代码()));
                                     return;
                                 }
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.互动.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.互动.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 运作) {
                             if (键名分割[2].equals(元素)) {
                                 if (键名分割[2].equals("beacon") && 键名分割[3].equals("power_select")) {
-                                    可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.互动.获取格式代码()));
+                                    可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.互动.获取格式代码()));
                                     return;
                                 }
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.运作.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.运作.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 危险方块) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.危险方块.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.危险方块.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 农作物) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.农作物.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.农作物.获取格式代码()));
                                 return;
                             }
                         }
-                        可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.方块_其它.获取格式代码()));
+                        可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.方块_其它.获取格式代码()));
                         return;
                     }
                     case "enchant", "particle" -> {
-                        可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.魔咒.获取格式代码()));
+                        可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.魔咒.获取格式代码()));
                         return;
                     }
                     case "entity" -> {
                         if (键名分割[2].equals("generic") || 键名分割[2].equals("player")) {
                             if (键名分割[3].equals("attack")) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.生物.玩家.攻击.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.生物.玩家.攻击.获取格式代码()));
                                 return;
                             }
                             for (String 元素 : 受伤) {
                                 if (键名分割[3].equals(元素)) {
-                                    可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.生物.玩家.受伤.获取格式代码()));
+                                    可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.生物.玩家.受伤.获取格式代码()));
                                     return;
                                 }
                             }
-                            可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.生物.玩家.玩家_其它.获取格式代码()));
+                            可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.生物.玩家.玩家_其它.获取格式代码()));
                             return;
                         }
                         for (String 元素 : 友好生物) {
                             if (键名分割[2].equals(元素)) {
                                 if (键名分割[2].equals("chicken") && Configuration.配置项.iKun彩蛋) {
-                                    可返回回调信息.setReturnValue(Text.translatable("subtitles.entity.kun." + 键名分割[3]).setStyle(this.text.getStyle().withColor(TextColor.fromFormatting(Formatting.GRAY)).withBold(true)));
+                                    可返回回调信息.setReturnValue(Text.translatable("subtitles.entity.kun." + 键名分割[3]).setStyle(字幕文本.getStyle().withColor(TextColor.fromFormatting(Formatting.GRAY)).withBold(true)));
                                     return;
                                 }
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.生物.被动生物.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.生物.被动生物.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 中立生物) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.生物.中立生物.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.生物.中立生物.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 敌对生物) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.生物.敌对生物.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.生物.敌对生物.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 头目生物) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.生物.头目生物.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.生物.头目生物.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 载具) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.载具.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.载具.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 弹射物) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.弹射物.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.弹射物.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 爆炸物) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.爆炸物.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.爆炸物.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 装饰品) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.装饰品.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.装饰品.获取格式代码()));
                                 return;
                             }
                         }
-                        可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.实体_其它.获取格式代码()));
+                        可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.实体_其它.获取格式代码()));
                         return;
                     }
                     case "event" -> {
-                        可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.实体.生物.敌对生物.获取格式代码()));
+                        可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.实体.生物.敌对生物.获取格式代码()));
                         return;
                     }
                     case "item" -> {
                         for (String 元素 : 武器) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.物品.武器.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.物品.武器.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 防具) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.物品.防具.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.物品.防具.获取格式代码()));
                                 return;
                             }
                         }
                         for (String 元素 : 工具) {
                             if (键名分割[2].equals(元素)) {
-                                可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.物品.工具.获取格式代码()));
+                                可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.物品.工具.获取格式代码()));
                                 return;
                             }
                         }
-                        可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.物品.物品_其它.获取格式代码()));
+                        可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.物品.物品_其它.获取格式代码()));
                         return;
                     }
                     case "ui" -> {
-                        可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.方块.互动.获取格式代码()));
+                        可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.方块.互动.获取格式代码()));
                         return;
                     }
                 }
             }
         }
-        可返回回调信息.setReturnValue(((MutableText) this.text).formatted(Configuration.配置项.基本颜色设置.其它.获取格式代码()));
+        可返回回调信息.setReturnValue(字幕文本.formatted(Configuration.配置项.基本颜色设置.其它.获取格式代码()));
     }
 }
